8af307a68beed769e7c512e335d966d0abd816b7
Wenbin <1539785265@qq.com>
2018-04-03 11:55:40 +0800

    游戏加速测试demo
    游戏加速测试demo

--- newccgame/newccgame/ccgame/ccgame_interface.lua
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/lua
-module("turbo.ccgame.ccgame_interface", package.seeall)
-
-local LuciJson = require("luci.jsonc")
-local LuciUtil = require("luci.util")
-local string, tonumber, pcall, type, table = string, tonumber, pcall, type, table
-
-require "turbo.ccgame.ccgame"
-require "turbo.ccgame.ccgame_util"
